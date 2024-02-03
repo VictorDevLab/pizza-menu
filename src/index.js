67472081820cfa5d993a8cb6da825712ffd99c0d
Victor Koome <koomev786@gmail.com>
2024-02-03 22:29:22 +0300

    Delete all files and start project from scratch

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello Worldio</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
